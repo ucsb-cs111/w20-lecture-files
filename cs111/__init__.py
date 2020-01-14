493862f92929ffb4193872a0cac800b537f73ed9
John R. Gilbert <gilbert@cs.ucsb.edu>
2020-01-14 13:19:29 -0800

    updated __init__

--- cs111/__init__.py
+++ cs111/__init__.py
@@ -1,7 +1,7 @@
 # use "import cs111 as cs" to run the cs111 course files
 
 # Lecture 1 and lecture 2
-from cs111.temperature import make_A_2D
+from cs111.temperature import make_A
 from cs111.temperature import make_A_small
 from cs111.temperature import make_b
 from cs111.temperature import make_b_small
