6621fbe1779c36b25583585d0df8bd61d3b6064f
John R. Gilbert <gilbert@cs.ucsb.edu>
2020-02-11 11:23:39 -0800

    files from 2/11/20

--- cs111/__init__.py
+++ cs111/__init__.py
@@ -1,20 +1,19 @@
 # use "import cs111" to use the cs111 course python functions
 
-# Lectures 1-2
 from cs111.temperature import make_A
 from cs111.temperature import make_A_small
 from cs111.temperature import make_b
 from cs111.temperature import make_b_small
 from cs111.temperature import radiator
 
-# Lectures 3-5
 from cs111.LU import LUfactor
 from cs111.LU import Lsolve
 from cs111.LU import Usolve
 from cs111.LU import LUsolve
 
-# Lecture 6
 from cs111.iterative import Jsolve
 from cs111.iterative import CGsolve
+
+from cs111.hilbert import hilbert
 
 from cs111.floatingpoint import print_float64
